8c7e247f9e075859c64671c3f1484bb1c1bf2704
Micha-8 <151458291+Micha-8@users.noreply.github.com>
2024-01-24 19:07:12 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from info import characters, storyline
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import json
 
+
 class CharactersStats:
     def __init__(self, intellect, agility, strength, name, description):
         self.intellect = intellect
@@ -11,41 +12,41 @@ class CharactersStats:
         self.description = description
         self.inventory = []
 
-    def intellect_up(self, count_new_intellect: int) -> int:
+    def intellect_up(self, count_new_intellect: int) -> None:
         self.intellect = self.intellect + count_new_intellect
 
-    def agility_up(self, count_new_agility: int) -> int:
+    def agility_up(self, count_new_agility: int) -> None:
         self.agility = self.agility + count_new_agility
 
-    def strength_up(self, count_new_strength: int) -> int:
+    def strength_up(self, count_new_strength: int) -> None:
         self.strength = self.strength + count_new_strength
 
     def add_to_inventory(self, item: str):
         self.inventory.append(item)
 
 
-bitter_knight = CharactersStats(1, 3, 5, f'{characters['bitter_knight']['name']}',
-                                f'{characters['bitter_knight']['description']}')
+bitter_knight = CharactersStats(1, 3, 5, f"{characters['bitter_knight']['name']}",
+                                f"{characters['bitter_knight']['description']}")
 
-a_brilliant_mind = CharactersStats(5, 2, 2, f'{characters['a_brilliant_mind']['name']}',
-                                   f'{characters['a_brilliant_mind']['description']}')
+a_brilliant_mind = CharactersStats(5, 2, 2, f"{characters['a_brilliant_mind']['name']}",
+                                   f"{characters['a_brilliant_mind']['description']}")
 
-child_of_thieves = CharactersStats(3, 5, 2, f'{characters['child_of_thieves']['name']}',
-                                   f'{characters['child_of_thieves']['description']}')
+child_of_thieves = CharactersStats(3, 5, 2, f"{characters['child_of_thieves']['name']}",
+                                   f"{characters['child_of_thieves']['description']}")
 
 
 def choice_your_character(message):
     if message.text == 'Гениальный Разум':
-        user_character = CharactersStats(5, 2, 2, f'{characters['a_brilliant_mind']['name']}',
-                                         f'{characters['a_brilliant_mind']['description']}')
+        user_character = CharactersStats(5, 2, 2, f"{characters['a_brilliant_mind']['name']}",
+                                         f"{characters['a_brilliant_mind']['description']}")
 
     elif message.text == 'Печальный Рыцарь':
-        user_character = CharactersStats(1, 3, 5, f'{characters['bitter_knight']['name']}',
-                                         f'{characters['bitter_knight']['description']}')
+        user_character = CharactersStats(1, 3, 5, f"{characters['bitter_knight']['name']}",
+                                         f"{characters['bitter_knight']['description']}")
 
     elif message.text == 'Дитя Воров':
-        user_character = CharactersStats(3, 5, 2, f'{characters['child_of_thieves']['name']}',
-                                         f'{characters['child_of_thieves']['description']}')
+        user_character = CharactersStats(3, 5, 2, f"{characters['child_of_thieves']['name']}",
+                                         f"{characters['child_of_thieves']['description']}")
 
     return user_character
 
@@ -102,7 +103,7 @@ def check_user_character(message, user_character, error_func, next_step):
 
 
 def send_question(bot, message, num_question: str, markup):
-    bot.send_message(message.chat.id, f'{storyline[num_question]['question']}',
+    bot.send_message(message.chat.id, f"{storyline[num_question]['question']}",
                      parse_mode="HTML",
                      reply_markup=markup)
 
@@ -149,19 +150,19 @@ def add_variants_for_fourth_answer(user_character, markup):
 
 def send_aftereffect_cell_answer(bot, message, error_func, next_step):
     if message.text == storyline['2']['answers_and_aftereffects']['1']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['2']['answers_and_aftereffects']['1']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['2']['answers_and_aftereffects']['1']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['2']['answers_and_aftereffects']['2']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['2']['answers_and_aftereffects']['2']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['2']['answers_and_aftereffects']['2']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['2']['answers_and_aftereffects']['3']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['2']['answers_and_aftereffects']['3']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['2']['answers_and_aftereffects']['3']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['2']['answers_and_aftereffects']['4']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['2']['answers_and_aftereffects']['4']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['2']['answers_and_aftereffects']['4']['aftereffect']}")
         next_step(message)
 
     else:
@@ -171,19 +172,19 @@ def send_aftereffect_cell_answer(bot, message, error_func, next_step):
 def send_aftereffect_room_answer(bot, message, user_character, error_func, next_step):
     if message.text == storyline['3']['answers_and_aftereffects']['1']['answer']:
         send_photo(bot, message, 'game_media/library.jpg')
-        bot.send_message(message.chat.id, f'{storyline['3']['answers_and_aftereffects']['1']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['3']['answers_and_aftereffects']['1']['aftereffect']}")
         user_character.intellect_up(5)
         next_step(message)
 
     elif message.text == storyline['3']['answers_and_aftereffects']['2']['answer']:
         send_photo(bot, message, 'game_media/chest_find.jpg')
-        bot.send_message(message.chat.id, f'{storyline['3']['answers_and_aftereffects']['2']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['3']['answers_and_aftereffects']['2']['aftereffect']}")
         user_character.strength_up(5)
         next_step(message)
 
     elif message.text == storyline['3']['answers_and_aftereffects']['3']['answer']:
         send_photo(bot, message, 'game_media/chest_find.jpg')
-        bot.send_message(message.chat.id, f'{storyline['3']['answers_and_aftereffects']['3']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['3']['answers_and_aftereffects']['3']['aftereffect']}")
         user_character.agility_up(5)
         next_step(message)
 
@@ -193,19 +194,19 @@ def send_aftereffect_room_answer(bot, message, user_character, error_func, next_
 
 def send_aftereffect_door_answer(bot, message, main_markup, error_func, next_step):
     if message.text == storyline['4']['answers_and_aftereffects']['1']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['4']['answers_and_aftereffects']['1']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['4']['answers_and_aftereffects']['1']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['4']['answers_and_aftereffects']['2']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['4']['answers_and_aftereffects']['2']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['4']['answers_and_aftereffects']['2']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['4']['answers_and_aftereffects']['3']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['4']['answers_and_aftereffects']['3']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['4']['answers_and_aftereffects']['3']['aftereffect']}")
         next_step(message)
 
     elif message.text == storyline['4']['answers_and_aftereffects']['4']['answer']:
-        bot.send_message(message.chat.id, f'{storyline['4']['answers_and_aftereffects']['4']['aftereffect']}',
+        bot.send_message(message.chat.id, f"{storyline['4']['answers_and_aftereffects']['4']['aftereffect']}",
                          parse_mode="HTML",
                          reply_markup=main_markup
                          )
@@ -217,21 +218,21 @@ def send_aftereffect_door_answer(bot, message, main_markup, error_func, next_ste
 def send_aftereffect_helpers_answer(bot, message, user_character, error_func, next_step):
     if message.text == storyline['5']['answers_and_aftereffects']['1']['answer']:
         send_photo(bot, message, 'game_media/scepter_gift.jpg')
-        bot.send_message(message.chat.id, f'{storyline['5']['answers_and_aftereffects']['1']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['5']['answers_and_aftereffects']['1']['aftereffect']}")
         user_character.intellect_up(10)
         user_character.add_to_inventory('scepter')
         next_step(message)
 
     elif message.text == storyline['5']['answers_and_aftereffects']['2']['answer']:
         send_photo(bot, message, 'game_media/sword_gift.jpg')
-        bot.send_message(message.chat.id, f'{storyline['5']['answers_and_aftereffects']['2']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['5']['answers_and_aftereffects']['2']['aftereffect']}")
         user_character.strength_up(10)
         user_character.add_to_inventory('sword')
         next_step(message)
 
     elif message.text == storyline['5']['answers_and_aftereffects']['3']['answer']:
         send_photo(bot, message, 'game_media/cloak_gift.jpg')
-        bot.send_message(message.chat.id, f'{storyline['5']['answers_and_aftereffects']['3']['aftereffect']}')
+        bot.send_message(message.chat.id, f"{storyline['5']['answers_and_aftereffects']['3']['aftereffect']}")
         user_character.agility_up(10)
         user_character.add_to_inventory('cloak')
         next_step(message)
@@ -244,41 +245,41 @@ def send_aftereffect_final_answer(bot, message, user_character, error_func):
     if message.text == storyline['6']['answers_and_aftereffects']['1']['answer']:
         if user_character.intellect == 20:
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['1']['aftereffect']['win']}')
+                             f"{storyline['6']['answers_and_aftereffects']['1']['aftereffect']['win']}")
         else:
             send_photo(bot, message, 'game_media/lost_photo.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['1']['aftereffect']['lose']}')
+                             f"{storyline['6']['answers_and_aftereffects']['1']['aftereffect']['lose']}")
 
     elif message.text == storyline['6']['answers_and_aftereffects']['2']['answer']:
         if user_character.intellect >= 18 and 'scepter' in user_character.inventory:
             send_photo(bot, message, 'game_media/win_with_scepter.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['2']['aftereffect']['win']}')
+                             f"{storyline['6']['answers_and_aftereffects']['2']['aftereffect']['win']}")
         else:
             send_photo(bot, message, 'game_media/lost_photo.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['2']['aftereffect']['lose']}')
+                             f"{storyline['6']['answers_and_aftereffects']['2']['aftereffect']['lose']}")
 
     elif message.text == storyline['6']['answers_and_aftereffects']['3']['answer']:
         if user_character.strength >= 20 and 'sword' in user_character.inventory:
             send_photo(bot, message, 'game_media/win_with_sword.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['3']['aftereffect']['win']}')
+                             f"{storyline['6']['answers_and_aftereffects']['3']['aftereffect']['win']}")
         else:
             send_photo(bot, message, 'game_media/lost_photo.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['3']['aftereffect']['lose']}')
+                             f"{storyline['6']['answers_and_aftereffects']['3']['aftereffect']['lose']}")
 
     elif message.text == storyline['6']['answers_and_aftereffects']['4']['answer']:
         if user_character.agility >= 18 and 'cloak' in user_character.inventory:
             send_photo(bot, message, 'game_media/win_with_cloak.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['4']['aftereffect']['win']}')
+                             f"{storyline['6']['answers_and_aftereffects']['4']['aftereffect']['win']}")
         else:
             send_photo(bot, message, 'game_media/lost_photo.jpg')
             bot.send_message(message.chat.id,
-                             f'{storyline['6']['answers_and_aftereffects']['4']['aftereffect']['lose']}')
+                             f"{storyline['6']['answers_and_aftereffects']['4']['aftereffect']['lose']}")
 
     else:
         error_func(message)
